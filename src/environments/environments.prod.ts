49f62812d44e7c6ebe2e988db2082adb5ca74bc4
PasqualeF <85121853+PasqualeF@users.noreply.github.com>
2025-07-23 00:52:58 +0200

    add reservation

--- src/environments/environments.prod.ts
+++ src/environments/environments.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:8081',
-  backendUrl: 'http://195.201.238.79:8181/vbe/v1',
+  backendUrl: 'https://virtustaranto.duckdns.org/vbe/v1',
   strapiUrl: 'https://virtustaranto.duckdns.org',
   strapiApiToken: '0111a6be7f0cdc4bb68bc1837a4bafd3c478426a46450ee4a8f81d6e86138257f598571deba9f7f7f5d9abce35071e7ac0ba2d9cf243e9ec045bbb8a0bc229414d7610e5eb82426ee23461aaabbcdacfaf5a84fa9c19a0a6554d7885e1056c3975d04303646cc5f74dd484eb7d9702bc54e5aa81e00027529b032d1c4f6f90db', 
   apiVersion: 'v1'
